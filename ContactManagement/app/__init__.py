9a01574e3d9d22aa2463704afc24ee1809124ad5
Yonatan Huber <yonatan.huber@gmail.com>
2021-08-22 19:41:06 +0300

    3: Web Forms

--- ContactManagement/app/__init__.py
+++ ContactManagement/app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from config import Config
 
 chimera_app = Flask(__name__)
+chimera_app.config.from_object(Config)
 
 from app import routes
--- ContactManagement/app/routes.py
+++ ContactManagement/app/routes.py
@@ -1,14 +1,27 @@
 from app import chimera_app
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 @chimera_app.route('/')
 @chimera_app.route('/index')
 def index():
-    return "Welcome to ChiMeRA - Contact Management RESTfull App"
+    return render_template('index.html')
+
+@chimera_app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('User {} logged-in successfully, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    else:
+        flash('Login failed. Please resubmit correct Username and Password')
+    return render_template('login.html', title='Sign In', form=form)
 
 @chimera_app.route('/person')
-@chimera_app.route('/person/{id}')
+@chimera_app.route('/person/{id}',methods=['GET'])
 def person():
-    return "Welcome to ChiMeRA - Contact Management RESTfull App - person"
+    return render_template('person.html', id = id)
 #POST
 #PUT
 #DELETE
